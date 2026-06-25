3e7c450ad8718ebb23e1df3e250d27efd4cc6bab
AdhimulamBhargavSaiViswanath-05 <bhargavsaiadhimulam12@gmail.com>
2026-06-25 10:33:17 +0530

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite';
-import { loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import { loadEnv } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), 'VITE_');
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd(), 'VITE_');
 
-  return {
-    base: env.VITE_BASE_PATH || '/smart-pilgrim-companion/',
-    plugins: [react()],
-  };
-});
+//   return {
+//     base: env.VITE_BASE_PATH || '/smart-pilgrim-companion/',
+//     plugins: [react()],
+//   };
+// });
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+
+export default defineConfig({
+  plugins:[react()],
+  base:"/smart-pilgrim-companion/"
+})
