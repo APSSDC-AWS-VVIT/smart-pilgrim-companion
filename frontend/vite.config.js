71eb55d1c8c98b3c6a77d6ee531e3f1be29c58f4
ADHIMULAM BHARGAV SAI VISWANATH <bhargavsaiadhimulam12@gmail.com>
2026-06-27 11:04:07 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,15 @@
+// import { defineConfig } from 'vite';
+// import { loadEnv } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd(), 'VITE_');
+
+//   return {
+//     base: env.VITE_BASE_PATH || '/smart-pilgrim-companion/',
+//     plugins: [react()],
+//   };
+// });
 import { defineConfig } from 'vite';
 import { loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
